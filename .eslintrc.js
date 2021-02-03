336bba413e9daec69320e95bd17feb5d8e4f312f
Ahmed Twfiek <ahmed.twfiek8@gmail.com>
2021-02-03 13:14:58 +0200

    testing husky pre-commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,14 @@
 module.exports = {
-  extends: ["airbnb"],
+  extends: ['airbnb'],
+  plugins: ['react', 'jsx-a11y', 'import'],
+  env: {
+    browser: true,
+  },
   rules: {
-    "react/jsx-filename-extension": "off",
-    //You can override any rules you want
+    'react/jsx-filename-extension': 'off',
+    // suppress errors for missing 'import React' in files
+    'react/react-in-jsx-scope': 'off',
+    // allow jsx syntax in js files (for next.js project)
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], //should add ".ts" if typescript project
   },
 };
